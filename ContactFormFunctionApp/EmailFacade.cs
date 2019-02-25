800ed5a035ddd1684994426dfb4cb078808068d8
Robert Swilley <robert@rswilley.com>
2019-02-25 06:31:09 -0500

    update to cleaner code

---
+++ ContactFormFunctionApp/EmailFacade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace ContactFormFunctionApp
+{
+    public class EmailFacade
+    {
+        public static void SendEmail(SendEmailInput input, Configuration config)
+        {
+            var email = new MailMessage
+            {
+                IsBodyHtml = true,
+                From = new MailAddress(config.FromEmail, config.FromName)
+            };
+
+            email.To.Add(new MailAddress(config.SendToEmail));
+            email.ReplyToList.Add(input.Email);
+            email.Subject = config.SubjectPrefix + input.Subject;
+            email.Body = $"<p>{input.Message}</p>";
+
+            using (var client = new SmtpClient(config.EmailHost, Convert.ToInt32(config.EmailPort)))
+            {
+                client.Credentials = new NetworkCredential(config.SmtpUsername, config.SmtpPassword);
+                client.EnableSsl = true;
+
+                client.Send(email);
+            }
+        }
+    }
+}
---
+++ ContactFormFunctionApp/SendEmailInput.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactFormFunctionApp
+{
+    public class SendEmailInput
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
